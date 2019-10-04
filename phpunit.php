26d411b818fd90fd646a8ac39fc79eeb8105a2e1
tioncico <1067197739@qq.com>
2019-10-04 16:46:02 +0800

    完成redis事务

--- phpunit.php
+++ phpunit.php
@@ -2,4 +2,4 @@
 
 defined("REDIS_HOST") ?: define('REDIS_HOST','127.0.0.1');
 defined("REDIS_PORT") ?: define('REDIS_PORT',6379);
-defined("REDIS_AUTH") ?: define('REDIS_AUTH','easyswoole');
+defined("REDIS_AUTH") ?: define('REDIS_AUTH','');
