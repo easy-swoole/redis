3cbe578a229ec6a7f6de6d4f7c1cad369a794dba
DQ <hifangxcx@fujuhaofang.com>
2020-07-22 09:49:21 +0800

    fix 在设置序列化时，redis zincrby命令中member未进行性序列化。取出时进行了反序列化。

--- src/CommandHandel/ZInCrBy.php
+++ src/CommandHandel/ZInCrBy.php
@@ -1,33 +1,27 @@
 <?php
+
 namespace EasySwoole\Redis\CommandHandel;
 
 use EasySwoole\Redis\CommandConst;
 use EasySwoole\Redis\Redis;
 use EasySwoole\Redis\Response;
 
-class ZInCrBy extends AbstractCommandHandel
-{
-	public $commandName = 'ZInCrBy';
+class ZInCrBy extends AbstractCommandHandel {
+    public $commandName = 'ZInCrBy';
 
-
-	public function handelCommandData(...$data)
-	{
-		$key=array_shift($data);
+    public function handelCommandData(...$data) {
+        $key = array_shift($data);
         $this->setClusterExecClientByKey($key);
-        $increment=array_shift($data);
-		$member=array_shift($data);
+        $increment   = array_shift($data);
+        $member      = array_shift($data);
+        $member      = $this->serialize($member);
+        $command     = [CommandConst::ZINCRBY, $key, $increment, $member];
+        $commandData = array_merge($command, $data);
 
+        return $commandData;
+    }
 
-		        
-
-		$command = [CommandConst::ZINCRBY,$key,$increment,$member];
-		$commandData = array_merge($command,$data);
-		return $commandData;
-	}
-
-
-	public function handelRecv(Response $recv)
-	{
-		return $recv->getData();
-	}
+    public function handelRecv(Response $recv) {
+        return $recv->getData();
+    }
 }
